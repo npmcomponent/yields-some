663e8f616eab95b4441d77f9e6ceaf16d187500f
Amir Abu Shareb <amir.abu.shareb@me.com>
2012-11-10 00:15:29 +0200

    initial

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+# some
+
+  es5 some
+
+## Installation
+
+    $ component install yields/some
+
+## API
+
+### every(arr, fn)
+
+```javascript
+every([1, 'str', 3], function (el, i) {
+  return 'string' == typeof el;
+});
+// > true
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,31 @@
+
+/**
+ * some
+ */
+
+var some = [].some;
+
+/**
+ * test wether some elements in
+ * the array pass the test implemented
+ * by `fn`.
+ *
+ * example:
+ *
+ *          some([1, 'foo', 'bar'], function (el, i) {
+ *            return 'string' == typeof el;
+ *          });
+ *          // > true
+ *
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {bool}
+ */
+
+module.exports = function (arr, fn) {
+  if (some) return some.call(arr, fn);
+  for (var i = 0, l = arr.length; i < l; ++i) {
+    if (fn(arr[i], i)) return true;
+  }
+  return false;
+};
